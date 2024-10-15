757322b874eb7eb2bec3e2d9957e2773ae1fbab8
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-15 13:08:45 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,11 @@ export default function Home() {
 
 const Hero = () => {
   return(
-    <div>
-      <div>
-        <h1>Hello!</h1>
-        <div>
-          <h2>
-            I am
-          </h2>
+    <div className="hero-container flex flex-col w-screen items-center p-24">
+      <div className="greeting-container flex flex-col gap-8">
+        <h1 className="text-5xl font-extrabold text-center bg-gradient-to-r from-gradientl1 to-gradientr1 bg-clip-text text-transparent">Hello!</h1>
+        <div className="flex flex-row items-center gap-4">
+          <h2 className="text-3xl font-bold">I am</h2>
           <div>
             <h2>UI/UX Designer</h2>
             <h2>Frontend Developer</h2>
