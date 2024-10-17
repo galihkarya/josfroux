4c99fb0e42c79b70739584a0f03dc7cf2709ac3c
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-17 14:14:41 +0700

    try to disabling scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      noScroll: {
+        '.no-scrollbar::-webkit-scrollbar': { display: 'none' },
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        bgnav: "var(--bgnav)",
+        subtitlel: "var(--subtitlel)",
+        subtitler: "var(--subtitler)",
         gradientl1: "#64EDAE",
         gradientr1: "#088395",
+        bgcontainer: "var(--bgcontainer)", 
+        bordercolor: "#97979777" ,
       },
     },
   },
