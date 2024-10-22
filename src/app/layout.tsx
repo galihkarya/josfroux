de178c907c4640addd38b7adc62808857035360c
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-22 12:14:44 +0700

    add smooth scroll duration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth duration-1000">
       <head>
         <link
           rel="stylesheet"
